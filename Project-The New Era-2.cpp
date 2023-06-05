a4827c4886e5335dab90d45f3aef0d1702e65787
swayam thummar <127782240+swayamthummar@users.noreply.github.com>
2023-06-05 17:49:07 +0530

    Add files via upload

---
+++ Project-The New Era-2.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+class Student
+{
+	public:
+	int roll;
+	char name[100];
+};
+main()
+{
+	Student s1,s2;
+	cout<<"Enter your roll number->";
+	cin>>s1.roll;
+	cout<<"Enter your name->";
+	cin>>s1.name;
+	cout<<"Enter your roll number->";
+	cin>>s2.roll;
+	cout<<"Enter your name->";
+	cin>>s2.name;
+	
+	cout<<"---------------------------------------"<<endl;
+	
+	cout<<"Roll number ->"<<s1.name<<endl;
+	cout<<"Name ->"<<s1.name<<endl;
+	cout<<"Roll number ->"<<s2.name<<endl;
+	cout<<"Name ->"<<s2.name<<endl;
+}
